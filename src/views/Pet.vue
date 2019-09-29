c00a968fdb5bcdaa33704714c7ff7f630c6e1291
jatasya <vikas.cta89@gmail.com>
2019-09-29 16:44:15 +0530

    displaying of dog details done

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,8 +1,9 @@
 <template lang="html">
   <div id="pet`">
     <h2>Pet</h2>
-    <span>{{$route.params.species}}</span>
-    <span>{{$route.params.id}}</span>
+    <h4>{{animal.name}} is of species ({{$route.params.species}})</h4>
+    <p>Age: {{animal.age}} years old</p>
+    <p>Breed: {{animal.breed}}</p>
   </div>
 </template>
 
@@ -11,11 +12,20 @@ import {mapState} from 'vuex'
 
 export default {
   name: 'Pet',
+  data() {
+    return {
+      animal: {}
+    }
+  },
   computed: {
     ...mapState([
       'cats',
       'dogs'
     ])
+  },
+  mounted() {
+    const animal = this[this.$route.params.species][this.$route.params.id];
+    this.animal = animal;
   }
 }
 </script>
